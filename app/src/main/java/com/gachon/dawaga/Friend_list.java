3912042785f212365c7f4d65eed97e4b1f82469a
woogie96 <fjdi789@gachon.ac.kr>
2021-05-19 09:12:03 +0900

    edit friend list

--- app/src/main/java/com/gachon/dawaga/Friend_list.java
+++ app/src/main/java/com/gachon/dawaga/Friend_list.java
@@ -3,11 +3,15 @@ package com.gachon.dawaga;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,12 +19,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public class Friend_list extends AppCompatActivity implements MyRecyclerAdapter.MyRecyclerViewClickListener {
@@ -31,12 +38,43 @@ public class Friend_list extends AppCompatActivity implements MyRecyclerAdapter.
     Toolbar toolbar;
 
     final MyRecyclerAdapter adapter = new MyRecyclerAdapter(dataList);
-    static int i=0;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference productRef = db.collection("Friend");
+        productRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String uid2 = (String) document.getId();
+                        if (uid.equals(uid2)) {
+                            Map<String, Object> tempFriendList = document.getData();
+                            for (String key : tempFriendList.keySet()) {
+                                String value = document.getString(key);
+                                dataList.add(new ItemData(sample[0],value,""));
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "파이어베이스 연동 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        adapter.setOnClickListener(this);
 
         toolbar = findViewById(R.id.toolbar_friend);
         //상단 툴바 설정
@@ -44,31 +82,25 @@ public class Friend_list extends AppCompatActivity implements MyRecyclerAdapter.
         getSupportActionBar().setDisplayShowCustomEnabled(true); // 커스터마이징 하기 위해 필요
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#55e6c3"))); // 툴바 배경색
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        dataList.add(new ItemData(sample[0],"김상욱",""));
-        dataList.add(new ItemData(sample[0],"이동훈",""));
-        dataList.add(new ItemData(sample[0],"조승환",""));
-        dataList.add(new ItemData(sample[0],"박태현",""));
-
-        for (int i=0; i<10; i++) {
-            dataList.add(new ItemData(sample[0], "친구 "+i, ""));
-        }
-
-        recyclerView.setAdapter(adapter);
-        adapter.setOnClickListener(this);
-
         Button button = findViewById(R.id.appendFriend);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_main = new Intent (Friend_list.this, AppendFriend.class);
                 startActivity(intent_main);
+                finish();
+            }
+        });
+
+        Button button1 = findViewById(R.id.loadFriend);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    recyclerView.setAdapter(adapter);
             }
         });
     }
+
 
     @Override
     public void onItemClicked(int position) {
