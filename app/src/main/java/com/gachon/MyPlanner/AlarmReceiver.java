dbd3f00819954b418a28e9f91a71b2bc1f3a1a14
MinjaeKim1 <mjkim0415@icloud.com>
2021-11-06 22:28:44 -0400

    project commit

---
+++ app/src/main/java/com/gachon/MyPlanner/AlarmReceiver.java
@@ -0,0 +1,82 @@
+package com.gachon.MyPlanner;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //declare notificationManager and builder
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        String appointment_title = intent.getStringExtra("title");
+
+        String channelName ="DAWAGA";
+        String description = "There is an upcoming appointment soon.";
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, 0);
+
+        // when above OREO API 26, channel is needed
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground); // when using minimap where API>oreo environment ,occurs error!
+            int importance = NotificationManager.IMPORTANCE_HIGH; // notification with alarm and sound
+
+            NotificationChannel channel = new NotificationChannel("default", channelName, importance);
+            channel.setDescription(description);
+
+            if (notificationManager != null) {
+                // register notification channel into system
+                notificationManager.createNotificationChannel(channel);
+            }
+        }else builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        builder.setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.dawaga_logo)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setVibrate(new long[]{0,2000,1000,2000})
+                .setTicker("{Time to watch some cool stuff!}")
+                .setContentTitle("DAWAGA")
+                .setContentText("It's time for "+ appointment_title+ ". click to check!")
+                .setContentInfo("INFO")
+                .setContentIntent(pendingIntent);
+
+        Notification notification= builder.build();
+        notificationManager.notify(1,notification);
+
+//        if (notificationManager != null) {
+//
+//            // Run notificationManager
+//            notificationManager.notify(1234, builder.build());
+//
+//            Calendar nextNotifyTime = Calendar.getInstance();
+//
+//            // 내일 같은 시간으로 알람시간 결정
+//            nextNotifyTime.add(Calendar.DATE, 1);
+//
+//            //  Preference에 설정한 값 저장
+//            SharedPreferences.Editor editor = context.getSharedPreferences("daily alarm", MODE_PRIVATE).edit();
+//            editor.putLong("nextNotifyTime", nextNotifyTime.getTimeInMillis());
+//            editor.apply();
+//
+//            Date currentDateTime = nextNotifyTime.getTime();
+//            String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
+//            Toast.makeText(context.getApplicationContext(),"다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
+//        }
+    }
+}
---
+++ app/src/main/java/com/gachon/MyPlanner/Friend_list.java
@@ -0,0 +1,126 @@
+package com.gachon.MyPlanner;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class Friend_list extends AppCompatActivity implements MyRecyclerAdapter.MyRecyclerViewClickListener {
+
+    ArrayList<ItemData> dataList = new ArrayList<>();
+    int[] sample = {R.drawable.ic_launcher_foreground};
+
+    Toolbar toolbar;
+
+    final MyRecyclerAdapter adapter = new MyRecyclerAdapter(dataList);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_friends_list);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference productRef = db.collection("Friend");
+        productRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String uid2 = (String) document.getId();
+                        if (uid.equals(uid2)) {
+                            Map<String, Object> tempFriendList = document.getData();
+                            for (String key : tempFriendList.keySet()) {
+                                String value = document.getString(key);
+                                dataList.add(new ItemData(sample[0],value,""));
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "파이어베이스 연동 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        adapter.setOnClickListener(this);
+
+        toolbar = findViewById(R.id.toolbar_friend);
+        //상단 툴바 설정
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowCustomEnabled(true); // 커스터마이징 하기 위해 필요
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#55e6c3"))); // 툴바 배경색
+
+        Button button = findViewById(R.id.appendFriend);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_main = new Intent (Friend_list.this, AppendFriend.class);
+                startActivity(intent_main);
+                finish();
+            }
+        });
+
+        Button button1 = findViewById(R.id.loadFriend);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    recyclerView.setAdapter(adapter);
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemClicked(int position) {
+        //Toast.makeText(getApplicationContext(), "Item : "+position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onTitleClicked(int position) {
+        //Toast.makeText(getApplicationContext(), "Title : "+position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onContentClicked(int position) {
+        //Toast.makeText(getApplicationContext(), "Content : "+position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onImageViewClicked(int position) {
+        //Toast.makeText(getApplicationContext(), "Image : "+position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onItemLongClicked(int position) {
+        //adapter.remove(position);
+        //Toast.makeText(getApplicationContext(),
+                //dataList.get(position).getTitle()+" is removed",Toast.LENGTH_SHORT).show();
+    }
+}
